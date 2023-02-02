dee16f27d367122ee9fdadef8203c87a683b2c8d
Sofiermas <Volserma@yandex.ru>
2023-02-02 18:22:57 +0300

    Выполнить ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,31 @@
-const editButtonProfile = document.querySelector('.button_edit-profile');
+const editButtonProfile = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
-const closeButtonPopup = popup.querySelector('.button_popup-close');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_type_name'); 
-let jobInput = document.querySelector('.popup__input_type_job'); 
-let nameProfile = document.querySelector('.profile__name'); 
+const closeButtonPopup = popup.querySelector('.popup__btn-close');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
 
 
-const toggleOpenPopup = () => {
-   popup.classList.toggle("popup_opened");
+function handleEditButtonProfileClick() {
+   popup.classList.add('popup_opened')
+   nameInput.value = nameProfile.textContent;
+   jobInput.value = jobProfile.textContent;
 }
 
-const handleEditButtonProfileClick = () => {
-   toggleOpenPopup();
+function handleCloseButtonPopupClick() {
+   popup.classList.remove('popup_opened')
 }
 
-const handleCloseButtonPopupClick = () => {
-   toggleOpenPopup();
-}
-
-const handleOverlayClick = (event) => {
-   if (event.target === event.currentTarget)
-
-      toggleOpenPopup();
-}
 
 editButtonProfile.addEventListener('click', handleEditButtonProfileClick);
 closeButtonPopup.addEventListener('click', handleCloseButtonPopupClick);
-popup.addEventListener('click', handleOverlayClick);
 
+
+let formElement = document.querySelector('.popup__form');
 
 function formSubmitHandler(event) {
    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-   // nameInput.getAttribute('value');
-   // jobInput.getAttribute('value');
-   // Получите значение полей из свойства value
 
    nameProfile.textContent = nameInput.value;
    jobProfile.textContent = jobInput.value;
@@ -48,4 +37,18 @@ formElement.addEventListener('submit', formSubmitHandler);
 
 
 
+/* Прошлая версия 
 
+   const toggleOpenPopup = () => {
+   popup.classList.toggle("popup_opened");
+   nameInput.value = nameProfile.textContent;
+   jobInput.value = jobProfile.textContent;
+}
+
+ const handleEditButtonProfileClick = () => {
+   toggleOpenPopup();
+ }
+
+const handleCloseButtonPopupClick = () => {
+  toggleOpenPopup();
+ } */
